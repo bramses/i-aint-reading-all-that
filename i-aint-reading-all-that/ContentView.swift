1a7c1d0219b3ee23e898260c30fc7c27ab687dda
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-04-13 15:56:14 -0400

    Initial Commit

---
+++ i-aint-reading-all-that/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  i-aint-reading-all-that
+//
+//  Created by Bram Adams on 4/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ i-aint-reading-all-that/i_aint_reading_all_thatApp.swift
@@ -0,0 +1,17 @@
+//
+//  i_aint_reading_all_thatApp.swift
+//  i-aint-reading-all-that
+//
+//  Created by Bram Adams on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct i_aint_reading_all_thatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
