387757f314df5912c28bca2ff3cc576d30096612
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-04-13 22:23:38 -0400

    init

---
+++ i-aint-reading-all-that/APIManager.swift
@@ -0,0 +1,85 @@
+//
+//  APIManager.swift
+//  i-aint-reading-all-that
+//
+//  Created by Bram Adams on 4/13/23.
+//
+
+import Foundation
+import Alamofire
+import KeychainSwift
+
+class APIManager {
+    static let shared = APIManager()
+    private let openAIURL = "https://api.openai.com/v1"
+    private let keychain = KeychainSwift()
+    private var apiKey: String {
+        get {
+            return keychain.get("openai_api_key") ?? ""
+        }
+        set {
+            keychain.set(newValue, forKey: "openai_api_key")
+        }
+    }
+
+    private init() {}
+
+    func transcribeAudio(fileURL: URL, completion: @escaping (Result<String, Error>) -> Void) {
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(apiKey)"
+        ]
+
+        let url = "\(openAIURL)/audio/transcriptions"
+        
+
+        AF.upload(multipartFormData: { multipartFormData in
+            multipartFormData.append(fileURL, withName: "file")
+            multipartFormData.append("whisper-1".data(using: .utf8)!, withName: "model")
+        }, to: url, headers: headers).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                if let json = value as? [String: Any], let transcription = json["text"] as? String {
+                    completion(.success(transcription))
+                } else {
+                    print(value)
+                    completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func chatAPI(prompt: String, completion: @escaping (Result<String, Error>) -> Void) {
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(apiKey)",
+            "Content-Type": "application/json"
+        ]
+
+        let url = "\(openAIURL)/chat/completions"
+        let parameters: [String: Any] = [
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                ["role": "system", "content": "summarize the following text into bullet points"],
+                ["role": "user", "content": prompt]
+            ]
+        ]
+
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                if let json = value as? [String: Any], let choices = json["choices"] as? [[String: Any]], let firstChoice = choices.first, let message = firstChoice["message"] as? [String: Any], let text = message["content"] as? String {
+                    completion(.success(text))
+                } else {
+                    completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func summarizeToBulletPoints(text: String, completion: @escaping (Result<String, Error>) -> Void) {
+        chatAPI(prompt: "Summarize this text into bullet points: \(text)", completion: completion)
+    }
+}
--- i-aint-reading-all-that/ContentView.swift
+++ i-aint-reading-all-that/ContentView.swift
@@ -1,22 +1,132 @@
-//
-//  ContentView.swift
-//  i-aint-reading-all-that
-//
-//  Created by Bram Adams on 4/13/23.
-//
-
 import SwiftUI
+import AVFoundation
+import KeychainSwift
 
 struct ContentView: View {
+    @State private var isRecording = false
+    @State private var audioRecorder: AVAudioRecorder!
+    @State private var bulletPoints: String = ""
+    @State private var apiKey: String = ""
+    @State private var isApiKeyVisible = false
+
+    private let keychain = KeychainSwift()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                    if isApiKeyVisible {
+                        TextField("Paste your API key here", text: $apiKey)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .disableAutocorrection(true)
+                            .autocapitalization(.none)
+                    } else {
+                        SecureField("Paste your API key here", text: $apiKey)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .disableAutocorrection(true)
+                            .autocapitalization(.none)
+                    }
+
+                    Button(action: {
+                        isApiKeyVisible.toggle()
+                    }) {
+                        Image(systemName: isApiKeyVisible ? "eye.slash.fill" : "eye.fill")
+                            .foregroundColor(.blue)
+                    }
+                }
+                .padding()
+
+                Button(action: {
+                    keychain.set(apiKey, forKey: "openai_api_key")
+                }) {
+                    Text("Save API Key")
+                        .padding()
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+                .padding()
+
+            Text("i aint reading all that")
+                .font(.title)
+            ScrollView {
+                Text(bulletPoints)
+            }
+            .padding()
+
+            Button(action: {
+                if isRecording {
+                    stopRecording()
+                } else {
+                    startRecording()
+                }
+                isRecording.toggle()
+            }) {
+                Text(isRecording ? "Stop Recording" : "Start Recording")
+                    .padding()
+                    .background(isRecording ? Color.red : Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+            }
+            .padding()
+
+            Button(action: {
+                UIPasteboard.general.string = bulletPoints
+            }) {
+                Text("Copy to Clipboard")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+            }
+            .padding()
         }
-        .padding()
     }
+    
+    func startRecording() {
+            let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+
+            let settings = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 12000,
+                AVNumberOfChannelsKey: 1,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+
+            do {
+                audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+                audioRecorder.record()
+            } catch {
+                print("Could not start recording")
+            }
+        }
+
+        func stopRecording() {
+            audioRecorder.stop()
+
+            APIManager.shared.transcribeAudio(fileURL: audioRecorder.url) { result in
+                switch result {
+                case .success(let transcription):
+                    APIManager.shared.summarizeToBulletPoints(text: transcription) { result in
+                        switch result {
+                        case .success(let summary):
+                            DispatchQueue.main.async {
+                                bulletPoints = summary
+                            }
+                        case .failure(let error):
+                            print("Error summarizing text: \(error)")
+                        }
+                    }
+                case .failure(let error):
+                    print("Error transcribing audio: \(error)")
+                }
+            }
+        }
+
+        func getDocumentsDirectory() -> URL {
+            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            return paths[0]
+        }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
